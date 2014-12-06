31f45fca1f5eeaea99a3b70a7b68c894a566846f
Omar Essam <omar_2412@me.com>
2014-12-06 22:38:59 +0200

    Added .htaccess

--- application/controllers/Parser.php
+++ application/controllers/Parser.php
@@ -1,16 +1,15 @@
 <?php
 
 class Parser extends CI_Controller{
-	
+	public function index(){
+	     $this->load->view("test");
+	}
 	public function parse_text(){
 		
 		echo "Hello World";
 		
 	}
 	public function world_test(){
-	     $world = 1;
-	     if($word === 1)
-	          echo "HELLO";
 	}
 }
 
