4ab7c2e7efafd48d7f013f164ab95782be72ce6a
abdulaziz <abdulazizalaa@ymail.com>
2014-12-06 23:20:10 +0200

    parser class modification
    clear_code_lines added
    parser_text modified

--- application/controllers/Parser.php
+++ application/controllers/Parser.php
@@ -1,24 +1,45 @@
 <?php
 
-class Parser extends CI_Controller{
-	
-	public function parse_text($code_text){
-		
-		$code_array = explode("\n", $code_text);
-		
-		foreach ($code_array as $code_line) {
-			echo $code_line."\n";
+class Parser extends CI_Controller {
+
+	public function clear_code_lines($code_line) {
+		for ($i = 0; $i < strlen($code_line); $i++) {
+			if ($code_line[$i] == "(") {
+				return substr($code_line, 0, $i);
+			}
 		}
-		
+		return "";
 	}
-	public function index(){
-	     $this->load->view("test");
+
+	public function parse_text() {
+		$code_text = $this -> input -> post("text");
+		$code_array = explode("\n", $code_text);
+		$code_type_array = array();
+
+		foreach ($code_array as $code_line) {
+			if ($this->clear_code_lines($code_line) == "loop") {
+				$code_type_array[] = "loop";
+			} else {
+				$code_type_array[] = "function";
+			}
+		}
+
+		$i = 0;
+		foreach ($code_array as $code_line) {
+			echo $code_line;
+			echo $code_type_array[$i] . "<br>";
+			$i++;
+		}
+
 	}
-	public function validate(){
-	     print_r($_POST);
+
+	public function index() {
+		$this -> load -> view("test");
 	}
+
+	public function validate() {
+		print_r($_POST);
+	}
+
 }
-
-
-
 ?>
