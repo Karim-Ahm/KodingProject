7c568f41bc6db51975b9685c70b43d82d947b09d
abdulaziz <abdulazizalaa@ymail.com>
2014-12-07 20:05:19 +0200

    contains:
    get_Parameter function added
    parse_text function added

--- application/controllers/Parser.php
+++ application/controllers/Parser.php
@@ -2,6 +2,10 @@
 
 class Parser extends CI_Controller {
 
+	public function index() {
+		$this -> load -> view("GameView");
+	}
+
 	public function clear_code_lines($code_line) {
 		for ($i = 0; $i < strlen($code_line); $i++) {
 			if ($code_line[$i] == "(") {
@@ -10,35 +14,77 @@ class Parser extends CI_Controller {
 		}
 		return "";
 	}
-
-	public function parse_text() {
-		$code_text = $this -> input -> post("text");
-		$code_array = explode("\n", $code_text);
-		$code_type_array = array();
-
-		foreach ($code_array as $code_line) {
-			if ($this->clear_code_lines($code_line) == "loop") {
-				$code_type_array[] = "loop";
-			} else {
-				$code_type_array[] = "function";
+	
+	public function get_Parameter($line){
+		//$line = $this->input->get("line");
+		//echo $line;
+		$length = strlen($line);	
+		for($i = 0 ; $i<$length ; $i++){
+			if($line[$i] == '('){
+				for($j = $i ; $j<$length ; $j++){
+					if($line[$j] == ')'){
+						//echo "<br>".substr($line, $i+1, $j-$i-1);
+						return substr($line, $i+1, $j-$i-1);
+					}
+				}
 			}
 		}
+	}
 
+	public function parse_text() {
+		$code_array = array();	
+		$code_text = $this -> input -> post("text");
+		
+		$code_array_temp = explode("\n", $code_text);
+		$code_array_length = count($code_array_temp);
+		//echo $code_array_length;
+		
+		echo "<pre>";
+		//var_dump($code_array_temp);
+		echo "</pre>";
+		
 		$i = 0;
-		foreach ($code_array as $code_line) {
-			echo $code_line;
-			echo $code_type_array[$i] . "<br>";
-			$i++;
+		for ( ; $i<$code_array_length ; $i++) {
+			
+			if (strlen($code_array_temp[$i]) > 4) {
+				echo substr($code_array_temp[$i], 0,4)."<br>";
+				if (substr($code_array_temp[$i], 0,4) == "loop"){
+					$code_array[$i] = "loop";
+					$code_array[$i] = array("condition" => $this->get_Parameter($code_array_temp[$i]));
+					//echo $code_array[$i]["condition"];
+					$j = $i;
+					$k = 0;	
+					for( ; $j<$code_array_length ; $j++){
+						if($code_array_temp[$j] == "endloop;"){
+							$i = $j;
+							break;
+						}else{
+							$code_array[$i][$k++] = $code_array_temp[$j];
+							//echo $code_array[$j][$k]."<br>";
+						}
+					}
+				}else{
+					$code_array[$i] = $code_array_temp[$i];
+				}
+			} else if (strlen($code_array_temp[$i]) > 2) {
+				if (substr($code_array_temp[$i], 0,2) == "if") {
+
+				}else{
+					$code_array[$i] = $code_array_temp[$i];
+				}
+			}
 		}
+		
+		echo "<pre>";
+		var_dump($code_array);
+		echo "</pre>";
+		
+		/*$i = 0;
+		foreach ($code_array as $code_line) {
+			echo $code_line . "<br>";
+			$i++;
+		}*/
 
-	}
-
-	public function index() {
-		$this -> load -> view("test");
-	}
-
-	public function validate() {
-		print_r($_POST);
 	}
 
 }
