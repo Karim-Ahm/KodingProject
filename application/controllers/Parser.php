c286b05c704b984a7a125f8ca4e45a88ed79c974
Omar Essam <omar_2412@me.com>
2014-12-07 16:13:33 +0200

    Function checks

--- application/controllers/Parser.php
+++ application/controllers/Parser.php
@@ -10,9 +10,29 @@ class Parser extends CI_Controller{
 	public function index(){
 	     $this->load->view("test");
 	}
-	public function validate(){
-	     print_r($_POST);
+     
+     public function is_function($input){
+          $input = str_replace(";","",$input);
+          if(is_loop($input)) return false;
+          if(is_conditional($input)) return false;
+          return (in_array($input, $this->get_function()));
+     }
+     public function get_function(){
+          // GET ALL FUNCTIONS
+     }
+	public function is_loop($input){
+	     if(strpos("loop",$input) !== FALSE){
+	          return true;
+	     }
+	     else return false;
 	}
+	public function is_conditional($input){
+	     if(strpos("if",$input) !== FALSE){
+	          return true;
+	     }
+	     else return false;
+	}
+	
 }
 
 
